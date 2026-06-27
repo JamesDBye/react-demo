656bdd805357e793e002d7280285f1e3d0269067
James Bye <James_Bye@yahoo.co.uk>
2026-06-27 19:19:04 +0100

    replace the array with the API call using quarkus

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-    const projects = [
-          {
-            projectCode: "Windmill",
-            startDate: "2014-01-01",
-            endDate: "2024-12-31"
-          },
-          {
-            projectCode: "Woody",
-            startDate: "2026-05-01",
-            endDate: null
-          }
-  ]
+  const [projects, setProjects] = useState([])
+  useEffect(() => {
+                  fetch("http://localhost:8080/windmill")
+                    .then(response => response.json())
+                    .then(data => {
+                      setProjects(data)
+                    })
+                }, [])
   return (
-    <div>
-      <h1>Windmill Prototype</h1>
-      <div>
-          {projects.map(project => (
-            <p key={project.projectCode}>
-              {project.projectCode}
-            </p>
-          ))}
-      </div>
-    </div>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Project Code</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+              </tr>
+            </thead>
+
+            <tbody>
+
+              {projects.map(project => (
+                <tr key={project.projectCode}>
+                  <td>{project.projectCode}</td>
+                  <td>{project.startDate}</td>
+                  <td>{project.endDate ?? "Open"}</td>
+                </tr>
+              ))}
+
+            </tbody>
+          </table>
   )
 }
 
