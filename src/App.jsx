bf1d44b37510f82d97d64156ac7f82b5e72f071c
James Bye <James_Bye@yahoo.co.uk>
2026-06-27 18:51:50 +0100

    get properties of all elements of javascript array onto the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@ import './App.css'
 
 function App() {
     const projects = [
-    {
-      projectCode: "Windmill",
-      startDate: "2014-01-01",
-      endDate: "2024-12-31"
-    },
-    {
-      projectCode: "Woody",
-      startDate: "2026-05-01",
-      endDate: null
-    }
+          {
+            projectCode: "Windmill",
+            startDate: "2014-01-01",
+            endDate: "2024-12-31"
+          },
+          {
+            projectCode: "Woody",
+            startDate: "2026-05-01",
+            endDate: null
+          }
   ]
   return (
     <div>
       <h1>Windmill Prototype</h1>
-      <p>First project: {projects[0].projectCode}</p>
+      <div>
+          {projects.map(project => (
+            <p key={project.projectCode}>
+              {project.projectCode}
+            </p>
+          ))}
+      </div>
     </div>
   )
 }
