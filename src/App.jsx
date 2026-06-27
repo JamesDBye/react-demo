afb43d2e4fdd6a1c508727cb688038bc47a7bab5
James Bye <James_Bye@yahoo.co.uk>
2026-06-27 18:40:01 +0100

    get first element of javascript array onto the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
 
 function App() {
+    const projects = [
+    {
+      projectCode: "Windmill",
+      startDate: "2014-01-01",
+      endDate: "2024-12-31"
+    },
+    {
+      projectCode: "Woody",
+      startDate: "2026-05-01",
+      endDate: null
+    }
+  ]
   return (
     <div>
       <h1>Windmill Prototype</h1>
-      <p>Hello from React.</p>
+      <p>First project: {projects[0].projectCode}</p>
     </div>
   )
 }
