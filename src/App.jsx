bd7522781f5307e54c6774c2be5cf2c9f8fcd711
James Bye <James_Bye@yahoo.co.uk>
2026-07-07 22:56:24 +0100

    added javaScriptCribsheet and comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.css'
 import ProjectTable from './components/ProjectTable'
 
 function App() {
-  // State containing all projects
+  // State variable containing the array of projects returned by the backend.
   const [projects, setProjects] = useState([])
-  // selectedProject is a state variable that holds the currently selected project, e.g.'Woody'
+  
+  // Holds the currently selected project object.
+  // Initially null because no project has been selected yet. Later..
+  // // { projectCode: "Woody", startDate: "...", endDate: "..." }
   const [selectedProject, setSelectedProject] = useState(null)
   useEffect(() => {
                   fetch("http://localhost:8080/windmill")
@@ -15,22 +18,24 @@ function App() {
                     })
                 }, [])
   return (
-          // Pass the projects state variable as a prop to the ProjectTable component
-          <>
-              <ProjectTable
-                  projects={projects}
-                  onProjectSelected={setSelectedProject}
-              />
-              {/* beneath the table, display the selected project code or "None" if no project is selected */}
-              <p>
-                  Selected Project:
-                  {" "}
-                  {selectedProject
-                      ? selectedProject.projectCode
-                      : "None"}
-              </p>
-          </>         
-            )
+    <>
+      {/* Pass the projects state variable as a prop to the ProjectTable component 
+          
+          React doesn't let child components modify parent state directly.
+          Instead, App passes its state setter function (setSelectedProject) to ProjectTable (child).
+          ProjectTable calls it when the user selects a project/row.
+      */}
+      <ProjectTable
+        projects={projects}
+        onProjectSelected={setSelectedProject}
+      />
+      {/* beneath the table, display the selected project code or "None" if no project is selected */}
+      <p>
+        Selected Project:{' '}
+        {selectedProject ? selectedProject.projectCode : 'None'}
+      </p>
+    </>
+  )
 }
 
 export default App
